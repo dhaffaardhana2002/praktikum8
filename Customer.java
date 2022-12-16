cb0f4e8ae43d89b9b2f680adbe5c5695cf8f6982
Dhaffa Ardhana <ardhanadhaffa@gmail.com>
2022-12-16 11:08:42 +0700

    Added files

---
+++ Customer.java
@@ -0,0 +1,25 @@
+public class Customer {
+	String name;
+	String address;
+	public Order order;
+	public Item item;
+
+
+	public void tambahItem(Item e) {
+		this.item = e;
+	}
+
+	public void calcSubTotal() {
+		order.calcSubTotal();
+	}
+	public void calcTax() {
+		order.calcTax();
+	}
+	public void calcTotal() {
+		order.calcTotal();
+	}
+	public void calcTotalWeight() {
+		order.calcTotalWeight();
+	}
+
+}
